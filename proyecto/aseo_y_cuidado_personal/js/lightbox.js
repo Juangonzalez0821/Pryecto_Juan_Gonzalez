d1da614b971f1fa03a051ddf11636bcbd31ec45f
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2021-07-29 22:40:06 -0500

    Create lightbox.js

---
+++ proyecto/aseo_y_cuidado_personal/js/lightbox.js
@@ -0,0 +1,19 @@
+// const imagenes= document.querySelectorAll('.img-galeria')
+// const imagenesLigth = document.querySelector('.agregar-imagen')
+// const contenedorlight = document.querySelector('.imagen-light')
+
+// // console.log(imagenes)
+// // console.log(imagenesLigth)
+// // console.log(contenedorlight)
+
+// imagenes.forEach(imagen =>{
+//     imagen.addEventListener('click', ()=>{
+//         aparacerimagen(imagen.getAttribute('crs'))
+//     })
+// })
+
+// const aparacerimagen = (imagen)=>{
+//     imagenesLigth.src = imagen
+//     contenedorlight.classList.toggle('show')
+//     imagenesLigth.classList.toggle('showImage')
+// }
